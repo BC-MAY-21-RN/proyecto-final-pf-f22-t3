e7feac484a451d66753c088c6769f7cd2cd8aa6a
Mario Torres <tmario1508@gmail.com>
2022-04-21 22:20:24 -0500

    Add Tab Navigation

--- PET_ME/App.js
+++ PET_ME/App.js
@@ -1,32 +1,18 @@
 import {Image} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./src/navigation/Navigation";
 
 import ButtonPet from './src/components/ButtonPet';
 import BgPaws from './src/components/BgPaws';
 import Title from './src/components/Title';
+
 const App = () => {
   return (
-    <BgPaws opacity={0.78}>
-      <Image
-        style={{alignSelf: 'center'}}
-        source={require('./src/assets/logos/Brand.png')}
-      />
-      <Title text={'registro'} textType={'title'} />
-      <Title
-        text={'Ingresa para encontrar tu a mejor amigo'}
-        textType={'subTitle'}
-      />
-
-      <ButtonPet
-        text="Clic Me"
-        typeButton="D"
-        onPressFunction={() => console.log('my function')}
-      />
-    <Icon name="rocket" size={30} color="#900" />
-
-    </BgPaws>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 };
 
