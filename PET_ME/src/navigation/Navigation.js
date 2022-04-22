38b567703e547352879f5ae91d9d0427a4f20d2b
Mario Torres <tmario1508@gmail.com>
2022-04-22 16:25:09 -0500

    Add list of pets in home
    Co-authored-by:  EverardoTV <eveviera244@gmail.com>

--- PET_ME/src/navigation/Navigation.js
+++ PET_ME/src/navigation/Navigation.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import LoginScreen from '../screens/LoginScreen';
+
 import HomeTabs from './NavigationTab';
+
 
 const Stack = createNativeStackNavigator();
 
--- PET_ME/src/utils/colors.js
+++ PET_ME/src/utils/colors.js
@@ -1,7 +1,9 @@
 export default {
+  Gray_500: '#0B3129',
   Gray_400: '#64868E',
   Gray_300: '#98B4A6',
   Gray_200: '#D1E4D1',
   Gray_100: '#F3FBF1',
+  primary: '#3486FE',
   Orange: '#FF5842',
 };
