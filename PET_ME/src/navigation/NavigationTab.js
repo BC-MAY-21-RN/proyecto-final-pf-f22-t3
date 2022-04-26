480a52009014729c34fb29c6fe9abb6040547679
Mario Torres <tmario1508@gmail.com>
2022-04-25 19:37:36 -0500

    Update screen Home conflicts Add search input and icons filters
    Co-authored-by: Everardo Torres  <EverardoTV@users.noreply.github.com>

--- PET_ME/src/navigation/NavigationTab.js
+++ PET_ME/src/navigation/NavigationTab.js
@@ -48,7 +48,7 @@ export default function NavigationTab() {
             />
           ),
         }}
-      />
+      />      
       <Tab.Screen
         name="AddPet"
         component={AddPetScreen}
