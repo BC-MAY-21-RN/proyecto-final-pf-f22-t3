a8bf16d2296005678f4d9a21c5ae79801a7e9e7d
Jesus <chuchober@hotmail.com>
2022-04-27 10:14:05 -0500

    Add AddPetScreen component ButtonPet component was added type 'E' and Title component now receive style

--- PET_ME/src/screens/AddPetScreen.js
+++ PET_ME/src/screens/AddPetScreen.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import BgPaws from '../components/BgPaws';
@@ -16,3 +17,35 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
 });
+=======
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import BgPaws from '../components/BgPaws';
+import ButtonPet from '../components/ButtonPet';
+import Title from '../components/Title';
+import VectorImage from 'react-native-vector-image';
+
+const AddPetScreen = () => {
+  return (
+    <BgPaws opacity={0.78}>
+      <View style={styles.headerContainer}>
+        <ButtonPet text={'Cancelar'} typeButton={'E'} />
+        <Title style={{marginBottom: 0}} text={'1/2'} textType={'title'} />
+      </View>
+      <View>
+        <VectorImage source={require('../assets/img/image.svg')} />
+      </View>
+    </BgPaws>
+  );
+};
+
+export default AddPetScreen;
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
+>>>>>>> 9c4817c (Add AddPetScreen component)
