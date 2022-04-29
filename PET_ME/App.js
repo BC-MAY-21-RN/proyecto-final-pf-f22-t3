73729aba64397c7d9b9476d88b32cbbbb58043a0
Jesus <chuchober@hotmail.com>
2022-04-29 13:27:44 -0500

    AddPetScreenTwo design done conflicts

--- PET_ME/App.js
+++ PET_ME/App.js
@@ -5,7 +5,7 @@ import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 
 const App = () => {
-  return <AddPetScreenTwo />;
+  return <AddPetScreenOne />;
 };
 
 export default App;
