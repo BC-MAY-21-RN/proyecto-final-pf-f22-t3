42c0fefcaf6f701306e69994402e6e049b1ee9e6
Jesus <chuchober@hotmail.com>
2022-04-29 08:55:44 -0500

    AddPetScreenTwo design done

--- PET_ME/src/components/Checkbox.js
+++ PET_ME/src/components/Checkbox.js
@@ -1,12 +1,21 @@
+<<<<<<< HEAD
 import {StyleSheet, View} from 'react-native';
+=======
+import {StyleSheet, Text, View} from 'react-native';
+>>>>>>> 8f25396 (AddPetScreenTwo design done)
 import React from 'react';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
 import colors from '../utils/colors';
 import Title from './Title';
+<<<<<<< HEAD
 import {useField} from 'formik';
 
 const Checkbox = props => {
   const [field, meta, helpers] = useField(props);
+=======
+
+const Checkbox = props => {
+>>>>>>> 8f25396 (AddPetScreenTwo design done)
   const {label, style} = props;
   return (
     <View style={style}>
@@ -18,7 +27,11 @@ const Checkbox = props => {
           <Title style={{marginLeft: 10}} text={label} textType={'subTitle'} />
         }
         onPress={isChecked => {
+<<<<<<< HEAD
           helpers.setValue(isChecked);
+=======
+          console.log(isChecked);
+>>>>>>> 8f25396 (AddPetScreenTwo design done)
         }}
       />
     </View>
